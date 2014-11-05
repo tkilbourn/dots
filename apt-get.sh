0c143951625230adf9c8eb9192f1f455beb737ea
Tim Kilbourn <tkilbourn@google.com>
2014-11-04 22:06:13 -0800

    More tags support.
    Add exuberant-ctags to apt-get script.
    Add mappings to switch between windows.

--- apt-get.sh
+++ apt-get.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install build-essential git gitk rxvt-unicode-256color xmonad xmobar libghc-xmonad-contrib-dev libghc-xmonad-contrib-doc libghc-xmonad-contrib-prof vim
+sudo apt-get install build-essential git gitk rxvt-unicode-256color xmonad \
+    xmobar libghc-xmonad-contrib-dev libghc-xmonad-contrib-doc \
+    libghc-xmonad-contrib-prof vim exuberant-ctags
